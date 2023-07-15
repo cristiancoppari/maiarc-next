0934f786ff7a63f788fb16fb562b9c761b4e9c57
Cristian <cristian.coppari@gmail.com>
2023-07-15 20:03:32 -0300

    add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable @next/next/no-img-element */
+import icon_fb from "@/assets/images/facebook.svg";
+import icon_ig from "@/assets/images/instagram.svg";
+import icon_wpp from "@/assets/images/whatsapp.svg";
+
+import { Button } from "../UI/button";
+import Link from "next/link";
+
+type SvgImport = {
+  src: string;
+};
+
+const social: Array<{ icon: string; link: string }> = [
+  {
+    icon: (icon_fb as SvgImport).src,
+    link: "https://www.facebook.com",
+  },
+  {
+    icon: (icon_ig as SvgImport).src,
+    link: "https://www.instagram.com",
+  },
+  {
+    icon: (icon_wpp as SvgImport).src,
+    link: "https://www.whatsapp.com",
+  },
+];
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-col items-center py-12">
+      <p className="mb-8">LOGO</p>
+
+      <p className="mb-2 uppercase">Tulum | Ibiza | Miami | Punta del Este</p>
+
+      <p className="mb-4 uppercase">welcome.guests@maiarc.com | 12345678</p>
+
+      <div>
+        <Button variant="link" asChild>
+          <Link href="/privacy-policy" className="mb-8 text-xs">
+            Politicas de privacidad y Avisos Legales
+          </Link>
+        </Button>
+      </div>
+
+      <div className="flex items-center justify-center gap-4">
+        {social.map((item, index) => (
+          <a
+            href={item.link}
+            key={item.link}
+            className="flex h-10 w-10 items-center justify-center rounded-full border-2 p-2"
+          >
+            <img src={item.icon} alt={`icon-${index}`} className="h-full" />
+          </a>
+        ))}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/UI/PageLayout.tsx
+++ src/components/UI/PageLayout.tsx
@@ -2,36 +2,37 @@ import Head from "next/head";
 import { Montserrat, Lora } from "next/font/google";
 
 import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 const montserrat = Montserrat({
-    subsets: ["latin"],
-    variable: "--font-montserrat",
+  subsets: ["latin"],
+  variable: "--font-montserrat",
 });
 
 const lora = Lora({
-    subsets: ["latin"],
-    variable: "--font-lora",
+  subsets: ["latin"],
+  variable: "--font-lora",
 });
 
 interface PageLayoutProps {
-    children: React.ReactNode;
-    title: string;
+  children: React.ReactNode;
+  title: string;
 }
 
 function PageLayout({ children, title }: PageLayoutProps) {
-    return (
-        <>
-            <Head>
-                <title>{title}</title>
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
 
-            <div className={`${montserrat.variable} ${lora.variable}`}>
-                <Header />
-                {children}
-                {/* <Footer /> */}
-            </div>
-        </>
-    );
+      <div className={`${montserrat.variable} ${lora.variable}`}>
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default PageLayout;
