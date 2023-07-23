cb5dad0062ea6aaf2686282b92c3777a47fdd847
Cristian <cristian.coppari@gmail.com>
2023-07-23 18:54:26 -0300

    code refactor

--- src/components/UI/PageLayout.tsx
+++ src/components/UI/PageLayout.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { Montserrat, Lora } from "next/font/google";
 
 import { useContext } from "react";
 import { LangContext } from "@/context/langContext";
@@ -7,16 +6,6 @@ import { LangContext } from "@/context/langContext";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import WhatsappButton from "@/components/Buttons/WhatsappButton/WhatsappButton";
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  variable: "--font-montserrat",
-});
-
-const lora = Lora({
-  subsets: ["latin"],
-  variable: "--font-lora",
-});
 
 interface PageLayoutProps {
   children: React.ReactNode;
@@ -32,7 +21,7 @@ function PageLayout({ children, title }: PageLayoutProps) {
         <title>{title}</title>
       </Head>
 
-      <div className={`${montserrat.variable} ${lora.variable}`}>
+      <div>
         <Header content={content.header.content} />
         {children}
         <Footer content={content.footer.content} />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
+import { Montserrat, Lora } from "next/font/google";
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import LangContextProvider from "@/context/langContext";
 
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+const lora = Lora({
+  subsets: ["latin"],
+  variable: "--font-lora",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <LangContextProvider>
-      <Component {...pageProps} />
+      <main className={`${montserrat.variable} ${lora.variable}`}>
+        <Component {...pageProps} />
+      </main>
     </LangContextProvider>
   );
 }
