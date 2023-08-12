03dbc4fbad84883eab95b998da6b4744aa141d7f
Cristian <cristian.coppari@gmail.com>
2023-08-12 20:51:48 -0300

    fix rehydratation error

--- src/components/Sections/Newsletter/Newsletter.tsx
+++ src/components/Sections/Newsletter/Newsletter.tsx
@@ -12,9 +12,9 @@ const Newsletter = ({ title, text }: NewsletterProps) => {
     <section className="bg-zinc-800 py-8">
       <div className="container md:w-3/4">
         <h3 className="h3 mb-2 uppercase text-slate-100">{title}</h3>
-        <p className="mb-8 text-slate-100">
+        <div className="mb-8 text-slate-100">
           <ReactMarkdown>{text}</ReactMarkdown>
-        </p>
+        </div>
 
         <div className="mx-auto flex flex-col justify-center gap-4 sm:flex-row">
           <input type="text" className="w-full rounded-full p-2 px-4" placeholder="Ingresá tu correo electrónico" />
